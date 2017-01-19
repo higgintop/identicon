b6ad5382fe53afd67c86fd3b8f859fd5e1024a33
Allison Higginbotham <higgintop@gmail.com>
2017-01-19 13:18:24 -0600

    Task complete

--- lib/image.ex
+++ lib/image.ex
@@ -1,5 +1,5 @@
 defmodule Identicon.Image do
   # sole purpose is to hold our struct
   # default value of nil in hex
-  defstruct hex: nil
+  defstruct hex: nil, color: nil, grid: nil, pixel_map: nil
 end
